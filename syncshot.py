7b6f9b172be32c01117911159ba3813f061e9314
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-07-30 23:10:01 -0400

    2025-07-30T23:10:01.386704

--- syncshot.py
+++ syncshot.py
@@ -1,14 +1,27 @@
 import subprocess
+import logging
+
+from datetime import datetime
 
 
 def main():
-    local = is_local_dirty()
-    print("Is local dirty: ", local)
+    logging.info("Syncshot is running")
 
-    print("Syncshot")
+    logging.debug("Local has unstaged changes")
+    logging.debug("Staging local changes")
+    stage_local_changes()
+    logging.debug("Local changes staged")
+    logging.debug("Committing local changes")
+    commit_local_changes()
+    logging.debug("Local changes committed")
 
 
 def is_local_dirty() -> bool:
+    """
+    This will return True if git status has unstaged changes.
+    It will return False if all changes have been staged
+    """
+
     result = subprocess.run(
         ["git", "status", "--porcelain"], capture_output=True, text=True, check=True
     )
@@ -16,5 +29,29 @@ def is_local_dirty() -> bool:
     return bool(result.stdout.strip())
 
 
+def stage_local_changes():
+    """
+    Stage everything.
+    """
+
+    subprocess.run(["git", "add", "."], capture_output=False, check=True)
+
+
+def commit_local_changes():
+    """
+    Commit with timestamp
+    """
+
+    message = generate_commit_message()
+
+    subprocess.run(["git", "commit", "-m", message], capture_output=False, check=True)
+
+
+def generate_commit_message():
+    iso_now = datetime.now().isoformat()
+    return iso_now
+
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     main()
